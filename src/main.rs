2600e4fd84bf6d8cd871f933933234683802ba5e
Steve Mostovoy <stevemostovoysm@gmail.com>
2018-03-14 23:59:43 -0700

    Adapt glutin examples, add rustfft lib.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
+mod gfx;
+mod rpio;
+
 extern crate gl;
 extern crate glutin;
 extern crate sysfs_gpio;
 
 use glutin::GlContext;
-use sysfs_gpio::{Direction, Pin};
-use std::thread::sleep;
-use std::time::Duration;
 
 fn main() {
+    println!("=== Starting Music Visualizer ===");
     let mut events_loop = glutin::EventsLoop::new();
     let window = glutin::WindowBuilder::new()
         .with_title("Music Visualizer")
@@ -16,47 +17,23 @@ fn main() {
         .with_vsync(true);
     let gl_window = glutin::GlWindow::new(window, context, &events_loop).unwrap();
 
-    unsafe {
-        gl_window.make_current().unwrap();
-    }
+    let _ = unsafe { gl_window.make_current() };
 
-    unsafe {
-        gl::load_with(|symbol| gl_window.get_proc_address(symbol) as *const _);
-        gl::ClearColor(0.0, 1.0, 0.0, 1.0);
-    }
+    let gl = gfx::load(&gl_window);
 
-    let mut running = true;
-    while running {
-        events_loop.poll_events(|event| {
-            match event {
-                glutin::Event::WindowEvent{ event, .. } => match event {
-                    glutin::WindowEvent::Closed => running = false,
-                    glutin::WindowEvent::Resized(w, h) => gl_window.resize(w, h),
-                    _ => ()
-                },
-                _ => ()
-            }
-        });
-
-        unsafe {
-            gl::Clear(gl::COLOR_BUFFER_BIT);
+    events_loop.run_forever(|event| {
+        println!("{:?}", event);
+        match event {
+            glutin::Event::WindowEvent { event, .. } => match event {
+                glutin::WindowEvent::Closed => return glutin::ControlFlow::Break,
+                glutin::WindowEvent::Resized(w, h) => gl_window.resize(w, h),
+                _ => (),
+            },
+            _ => ()
         }
 
-        gl_window.swap_buffers().unwrap();
-    }
-}
-
-
-fn blink_pin(pin_number: u64) {
-    let pin = Pin::new(pin_number);
-    pin.set_direction(Direction::Out).unwrap();
-    pin.with_exported(|| {
-        loop {
-            pin.set_value(0).unwrap();
-            sleep(Duration::from_millis(200));
-
-            pin.set_value(1).unwrap();
-            sleep(Duration::from_millis(200));
-        }
-    }).unwrap();
+        gl.draw_frame([0.0, 1.0, 0.0, 1.0]);
+        let _ = gl_window.swap_buffers();
+        glutin::ControlFlow::Continue
+    });
 }
---
+++ src/rpio.rs
@@ -0,0 +1,17 @@
+use sysfs_gpio::{Direction, Pin};
+use std::thread::sleep;
+use std::time::Duration;
+
+fn blink_pin(pin_number: u64) {
+    let pin = Pin::new(pin_number);
+    pin.set_direction(Direction::Out).unwrap();
+    pin.with_exported(|| {
+        loop {
+            pin.set_value(0).unwrap();
+            sleep(Duration::from_millis(200));
+
+            pin.set_value(1).unwrap();
+            sleep(Duration::from_millis(200));
+        }
+    }).unwrap();
+}
