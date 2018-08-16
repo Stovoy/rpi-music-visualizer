18cb774e8cce54e39d5c2aa8d9cb68909bc35e6c
Steve Mostovoy <stevemostovoysm@gmail.com>
2018-08-15 22:04:42 -0700

    Don't set blinkt on self.

--- src/screen/hardware.rs
+++ src/screen/hardware.rs
@@ -6,17 +6,12 @@ use screen;
 
 pub struct HardwareScreen {
     mapper: led_mapper::LedDiskMapper,
-    blinkt: blinkt::Blinkt,
 }
 
 impl HardwareScreen {
     pub fn new() -> HardwareScreen {
-        let mut blinkt = Blinkt::with_spi(16_000_000, 144).unwrap();
-        blinkt.set_all_pixels_brightness(10.0);
-
         HardwareScreen {
             mapper: led_mapper::LedDiskMapper::new(),
-            blinkt,
         }
     }
 }
@@ -27,11 +22,14 @@ impl screen::Screen for HardwareScreen {
     fn render_from_texture(&self, gl: &gfx::gl::Gl, texture: u32) {
         let pixel_colors = self.mapper.map_from_texture(gl, texture);
 
+        let mut blinkt = Blinkt::with_spi(16_000_000, 255).unwrap();
+        blinkt.set_all_pixels_brightness(10.0);
+
         for (i, (r, g, b)) in pixel_colors.iter().enumerate() {
-            self.blinkt.set_pixel(i, *r, *g, *b);
+            blinkt.set_pixel(i, *r, *g, *b);
         }
 
-        self.blinkt.show().unwrap();
+        blinkt.show().unwrap();
     }
 
     fn uses_window(&self) -> bool {
