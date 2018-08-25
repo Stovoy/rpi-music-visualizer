8055b42aa50a9be68dbe1efd0bf287fa07390d64
Steve Mostovoy <stevemostovoysm@gmail.com>
2018-08-25 08:43:13 -0700

    Pin 24.

--- src/rpio.rs
+++ src/rpio.rs
@@ -5,7 +5,7 @@ use std::time::Duration;
 pub fn read_button(pin: u64) {
     let pin = Pin::new(pin);
     pin.set_direction(Direction::In).unwrap();
-    pin.set_active_low(true).unwrap();
+    pin.set_active_low(false).unwrap();
 
     pin.with_exported(|| {
         loop {
@@ -15,3 +15,4 @@ pub fn read_button(pin: u64) {
 
     }).unwrap();
 }
+echo 24 > /sys/class/gpio/export
